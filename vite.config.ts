af470f435d1284359efdbfb0eba1ec000c8acd6c
Deo.SiriliusVansaHavian <48593365+Xelszy@users.noreply.github.com>
2025-04-20 19:29:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, '.', 'VITE_');
     return {
       define: {
-        'process.env.API_KEY': JSON.stringify(env.API_KEY)
+        // Tidak perlu define di sini karena Vite akan mengekspos otomatis variabel dengan prefix VITE_
       },
       resolve: {
         alias: {
